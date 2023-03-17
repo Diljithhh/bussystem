86bf11730d8bb43f4ab16525da972bfb4e6a322f
Swapnil Swaraj Sinha <swapnil.sinha@muncho.in>
2023-03-17 23:50:53 +0530

    welcome uidone

--- lib/UI/LaunchScreen.dart
+++ lib/UI/LaunchScreen.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:ffi';
 
-import 'package:bussystem/UI/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:bussystem/UI/WelcomeScreen.dart';
 
 class LaunchWidget extends StatefulWidget {
   const LaunchWidget({super.key});
@@ -18,36 +20,81 @@ class _LaunchWidgetState extends State<LaunchWidget> {
     final size = MediaQuery.of(context).size;
 
     return Container(
-      height: double.maxFinite, width: double.maxFinite,
+      height: double.maxFinite,
+      width: double.maxFinite,
       color: Colors.red,
-        padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       child: Stack(
         // mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Positioned(left: 10,right: 10,top: 20,bottom: 20,
+          Positioned(
+            left: 10,
+            right: 10,
+            top: 20,
+            bottom: 20,
             child: Container(
               child: Image.asset('assets/images/moovbe.png'),
             ),
           ),
-          Positioned(left: 10,right: 10,bottom: 10,
-            child: Container(
+          Positioned(
+            left: 10,
+            right: 10,
+            bottom: 10,
+            child: CustomTextButton(buttonText: 'Get Started',
+              backgroundColor: Colors.white,
+              buttonTextColor: Colors.red,
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WelcomeScreen(),
+                    ));
+              },
               width: 300,
               height: 60,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(15)),
-              child: TextButton(onPressed: () {
-Navigator.push(context, MaterialPageRoute(builder: (context) =>WelcomeScreen() )
-);
-
-
-
-              }, child: Text(
-                style: const TextStyle(color: Colors.red,fontWeight: FontWeight.w500,fontSize: 20,),
-                'Get Started')),
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  final double width;
+  final double height;
+
+  final Color backgroundColor;
+  final Color buttonTextColor;
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  CustomTextButton({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.backgroundColor,
+    required this.buttonTextColor,
+    required this.buttonText,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+          color: backgroundColor, borderRadius: BorderRadius.circular(15)),
+      child: TextButton(
+          onPressed: onPressed,
+          child: Text(buttonText,
+              style: TextStyle(
+                color: buttonTextColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 20,
+              ),
+              )),
     );
   }
 }
--- lib/UI/WelcomeScreen.dart
+++ lib/UI/WelcomeScreen.dart
@@ -1,6 +1,6 @@
+import 'package:bussystem/UI/LaunchScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -14,7 +14,76 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: Center(child: Text('eee')),
+      child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween
+      ,
+        children: [
+          Stack(
+            children: [
+              Positioned(
+                  child: Container(
+                child: Image.asset(
+                  fit: BoxFit.cover,width: double.maxFinite,
+                  'assets/images/Rectangle.png'),
+              ))
+            ,Positioned(
+                  child: Container(
+                child: Image.asset(
+                  //fit: BoxFit.cover,
+                  width: double.maxFinite,
+                  'assets/images/Polygon.png'),
+              )),const Positioned( 
+                left: 20,
+                bottom: 0,top: 10,
+              
+                child: Center(
+                  child: Text('Welcome',style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,fontWeight: FontWeight.w700),),
+                )) ,const Positioned( 
+                left: 20,
+                bottom: 0,top: 100,
+              
+                child: Center(
+                  child: Text('Manage your Bus and Drivers',style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,fontWeight: FontWeight.w400),),
+                )) ],
+          )
+      ,  
+      //SizedBox(height: 10,),
+     Container(padding: const EdgeInsets.only(bottom: 100),
+       child: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [  CustomTextButton(
+          buttonText: 'Enter Username',
+          width: 300, height: 60, backgroundColor: Colors.grey.shade300,
+          buttonTextColor: Colors.black, onPressed: () {
+            
+          },)
+          ,
+          const SizedBox(height: 20,),
+          CustomTextButton(
+          buttonText: 'Enter Password',
+          width: 300, height: 60, backgroundColor: Colors.grey.shade300,
+          buttonTextColor: Colors.black, onPressed: () {
+            
+          },),
+          ],),
+     )
+        ,
+        Container(padding: const EdgeInsets.only(bottom: 60),
+          child: CustomTextButton(buttonText: 'Login',
+                backgroundColor: Colors.red,
+                buttonTextColor: Colors.white,
+                onPressed: () {
+                  
+                },
+                width: 300,
+                height: 60,
+              ),
+        ) ],
+      ),
     ));
   }
 }
